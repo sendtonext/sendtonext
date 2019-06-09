22d6bac09a7828927d0219a9e74c5f55dacabb21
sendtonext <sendtonext@gmail.com>
2019-06-10 03:55:59 +0900

    Updates

---
+++ precache-manifest.1839ce31283ee678f9264d32bbc03cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da037194b567494dac4fffc83d14ef17",
+    "url": "/sendtonext/index.html"
+  },
+  {
+    "revision": "c77fc111650c90a8a32f",
+    "url": "/sendtonext/static/css/main.ce09204d.chunk.css"
+  },
+  {
+    "revision": "f6ebc6c98200e79b81fd",
+    "url": "/sendtonext/static/js/2.5ae47fe4.chunk.js"
+  },
+  {
+    "revision": "c77fc111650c90a8a32f",
+    "url": "/sendtonext/static/js/main.2c004f95.chunk.js"
+  },
+  {
+    "revision": "205d0cefe068604b76cc",
+    "url": "/sendtonext/static/js/runtime~main.f41e3002.js"
+  },
+  {
+    "revision": "8b1dab2bef0a37a71990640332dcff30",
+    "url": "/sendtonext/static/media/memo.8b1dab2b.jpg"
+  }
+]);
